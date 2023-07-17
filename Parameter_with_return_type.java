fecbda2921c52a13a90836cbb4a5b68dcea4ae1d
Aditya7verma <129338439+Aditya7verma@users.noreply.github.com>
2023-07-17 21:32:38 +0530

    Add files via upload

---
+++ Parameter_with_return_type.java
@@ -0,0 +1,14 @@
+package Lac_9;
+
+public class Parameter_with_return_type {
+    public static void main(String[] args) {
+        System.out.println("Neyyy");
+        int a= 6;
+        int b=9;
+        System.out.println(add(a,b));
+    }
+    public static int add(int a,int b){
+        int c= a+b;
+        return c;
+    }
+}
